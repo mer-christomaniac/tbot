281d0ef0f4fd8741e5df53a787cda713fee074c2
Meriaddis Inchrist <meriaddisinchrist@gmail.com>
2026-02-05 20:22:09 +0300

    Add new user ID to people list

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ client = TelegramClient('my_session', api_id, api_hash)
 
 groq = Groq()
 
-people = [1365625365,7230622894,5035275727]
+people = [1365625365,7230622894,5035275727,1561900587]
 
 DB_PATH = "chat_history.db"
 HISTORY_LIMIT = 20
